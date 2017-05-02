21cfdcd8c591d9527542f4c459907720de5d9b92
Xinkai Chen <xinkai.chen@qq.com>
2017-05-02 16:15:38 +0800

    Fix numerous bugs

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ class DataBase {
     constructor(filePath) {
         this.filePath = filePath;
         this.knownPackages = [];
-        this.recentAddedPackageIds = [];
-        this.recentRemovedPackages = Object.create(null);
+        this.recentAddedPackages = Object.create(null); // repo/name: createTime
+        this.recentRemovedPackages = Object.create(null); // repo/name: pkg
 
         if (fs.existsSync(filePath)) {
             const str = fs.readFileSync(filePath, { encoding: "utf-8" });
@@ -22,14 +22,14 @@ class DataBase {
 
     load(content) {
         this.knownPackages = content.knownPackages;
-        this.recentAddedPackageIds = content.recentAddedPackageIds;
+        this.recentAddedPackages = content.recentAddedPackages;
         this.recentRemovedPackages = content.recentRemovedPackages;
     }
 
     save() {
         return writeJsonFile(this.filePath, {
             knownPackages: this.knownPackages,
-            recentAddedPackageIds: this.recentAddedPackageIds,
+            recentAddedPackages: this.recentAddedPackages,
             recentRemovedPackages: this.recentRemovedPackages,
         });
     }
@@ -53,12 +53,25 @@ const setMinus = (s1, s2) => {
 const array2Object = arr => {
     const o = Object.create(null);
     for (let pkg of arr) {
+        if (!(pkg.repo && pkg.name)) {
+            throw new Error(`${JSON.stringify(pkg)} is not a package`);
+        }
         // repo/name makes an id
         o[`${pkg.repo}/${pkg.name}`] = pkg;
     }
     return o;
 };
 
+
+const timeBefore = (t1, t2) => {
+    if (typeof t1 !== "number") {
+        throw new Error(`${t1} is not number`);
+    }
+    if (typeof t2 !== "number") {
+        throw new Error(`${t2} is not number`);
+    }
+    return t1 < t2
+};
 
 const main = () => {
     {
@@ -79,47 +92,55 @@ const main = () => {
         "/usr/bin/expac",
         ["--config", "./pacman.conf",
          "--timefmt", "%s",
-         "-S", String.raw`%r\t%n\t%a\t%b\t%u\t%d`,
+         "-S", String.raw`%r\t%e\t%n\t%a\t%b\t%u\t%d\t%v`,
         ], { encoding: "utf-8" });
 
     const packagesArray = result.stdout.trim().split("\n").map(line => {
-        const [repo, name, arch, time, url, desc] = line.split("\t");
-        return { repo, name, arch, time: +time, url, desc };
+        const [repo, base, name, arch, time, url, desc, ver] = line.split("\t");
+        return { repo, base: base === "(null)" ? null : base, name, arch, time: +time, url, desc, ver };
     });
     const packages = array2Object(packagesArray);
 
     const knownPackageIds = new Set(Object.keys(db.knownPackages));
     const packageIds = new Set(Object.keys(packages));
 
-    const recentAddedSet = setMinus(packageIds, knownPackageIds);
-    recentAddedSet.forEach(pkg => {
-        if (!db.recentAddedPackageIds.includes(pkg)) {
-            db.recentAddedPackageIds.push(pkg);
-        }
-    });
-    db.recentAddedPackageIds = db.recentAddedPackageIds.filter(id =>
-        packages[id].time >= timestamp - 3600 * 24 * 30
-    );
-
-    const recentRemovedSet = setMinus(knownPackageIds, packageIds);
-    recentRemovedSet.forEach(id => {
+    const justRemovedSet = setMinus(knownPackageIds, packageIds);
+    justRemovedSet.forEach(id => {
         db.recentRemovedPackages[id] = db.knownPackages[id];
         db.recentRemovedPackages[id].removeTime = timestamp;
     });
-    for (let id in db.recentRemovedPackages) {
-        if (id.removeTime < timestamp - 3600 * 24 * 30) {
+    for (let id of Object.keys(db.recentRemovedPackages)) {
+        if (timeBefore(db.recentRemovedPackages[id].removeTime, timestamp - 3600 * 24 * 30)) {
             delete db.recentRemovedPackages[id];
         }
     }
+
+    const justAddedSet = setMinus(packageIds, knownPackageIds);
+    justAddedSet.forEach(id => {
+        if (!(id in db.recentAddedPackages)) {
+            db.recentAddedPackages[id] = timestamp;
+        }
+    });
+    for (let id of Object.keys(db.recentAddedPackages)) {
+        if (timeBefore(db.recentAddedPackages[id], timestamp - 3600 * 24 * 30) ||
+            (!(id in packages))) {
+            delete db.recentAddedPackages[id];
+        }
+    }
+
     db.knownPackages = packages;
 
     return db.save().then(() => {
         const output = {
             timestamp,
             "recentAddedPackages":
-                db.recentAddedPackageIds
-                    .map(id => packages[id])
-                    .sort((pkg1, pkg2) => pkg2.time - pkg1.time),
+                Object.keys(db.recentAddedPackages)
+                    .map(id => {
+                        const r = packages[id];
+                        r.createTime = db.recentAddedPackages[id];
+                        return r;
+                    })
+                    .sort((pkg1, pkg2) => pkg2.createTime - pkg1.createTime),
 
             "recentRemovedPackages":
                 Object.values(db.recentRemovedPackages)
